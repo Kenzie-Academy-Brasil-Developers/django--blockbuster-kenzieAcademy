c2623661b0cb5b0e677ba0c2b0b363df9c1edf82
Francisco Stenico <francisco.stenico@gmail.com>
2022-12-07 18:00:24 -0300

    FEAT: Rota Login sem token de autenticação

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("users/", views.UserViews.as_view())
+    path("users/", views.UserViews.as_view()),
+    path("users/login/", views.LoginViews.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView, Request, Response, status
+from django.contrib.auth import authenticate
+
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, LoginSerializer
 
 
 class UserViews(APIView):
@@ -20,3 +22,22 @@ class UserViews(APIView):
         serializer = UserSerializer(users, many=True)
 
         return Response(serializer.data, status.HTTP_200_OK)
+
+
+class LoginViews(APIView):
+    """
+    View Responsável por realizar as tratativas de espostas referentes a sessão de usuários
+    """
+    def post(self, request: Request) -> Response:
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = authenticate(
+            username=serializer.validated_data["username"],
+            password=serializer.validated_data["password"],
+        )
+
+        if not user:
+            return Response({"detail": "Invalid credentials"}, status.HTTP_403_FORBIDDEN)
+
+        return Response({"detail": f"Welcome {user.username}!"}, status.HTTP_200_OK)
